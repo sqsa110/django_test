ce65e11cd15a2adde39eca8165c0332f14bd4424
lxl <1403276130@qq.com>
2017-11-10 21:25:18 +0800

    修改

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app import views as app_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', app_views.index)
 ]
